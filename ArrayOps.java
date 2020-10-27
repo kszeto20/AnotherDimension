be50c91e383b42f3a1ebdcaa7c343539f8a7cea6
Kirsten Szeto <kszeto20@stuy.edu>
2020-10-26 22:32:43 -0400

    Added sumRows, output still in progress

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,19 +1,19 @@
 public class ArrayOps{
-  public static void main(String[] args){
+  public static void main(String[] args) {
     System.out.println("hello world");
   }
 
-  public static int sum(int[] arr){
+  public static int sum(int[] arr) {
     int sum = 0;
-    for (int i = 0; i < arr.length; i++){
+    for (int i = 0; i < arr.length; i++) {
       sum += arr[i];
     }
     return sum;
   }
 
-  public static int largest(int[] arr){
+  public static int largest(int[] arr) {
     int largest = 0;
-    for (int i = 0; i < arr.length; i++){
+    for (int i = 0; i < arr.length; i++) {
       if (arr[i] > largest){
         largest = arr[i];
       }
@@ -21,5 +21,15 @@ public class ArrayOps{
     return largest;
   }
 
-    
+  public static int[] sumRows(int[][] matrix) {
+    int[] comboRow = new int[matrix.length];
+    for (int i = 0; i < matrix.length; i++) {
+      int rowSum = 0;
+      for (int j = 0; j < matrix[i].length; j++) {
+        rowSum += matrix[i][j];
+      }
+      comboRow[i] = rowSum;
+    }
+    return comboRow;
+  }
 }
--- Tester.java
+++ Tester.java
@@ -1,7 +1,13 @@
 public class Tester{
   public static void main(String[] args){
     int[] arr = {1, 3, 5, 7, 5, 30, 2, 5, 5, 10};
+    int[][] matrix = { {3, 4, 5, 6, 7}, {7, 6, 5, 4, 3}, {1, 2, 8, 9, 10} };
     System.out.println(ArrayOps.sum(arr));
     System.out.println(ArrayOps.largest(arr));
+
+    int[] a = ArrayOps.sumRows(matrix);
+    for (int i = 0; i < a.length; i++){
+      System.out.println("test" + a[i]);
+    }
   }
 }
