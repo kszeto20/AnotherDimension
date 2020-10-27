1c1b607fbf14871440463f7f647b2f95a6c04885
Kirsten Szeto <kszeto20@stuy.edu>
2020-10-27 13:03:17 -0400

    I changed the test cases

--- Tester.java
+++ Tester.java
@@ -1,28 +1,28 @@
 public class Tester{
   public static void main(String[] args){
-    int[] arr = {1, 3, 5, 7, 5, 30, 2, 5, 5, 10};
-    int[][] matrix = { {3, 4, 5, 6, 7}, {7, 6, 5, 4, 3}, {1, 2, 8, 9, 10, 11} };
-    int[][] matrix2 = { {1, 2, 3, 4, 5, 6}, {5, 5, 3, 3, 2, 3}, {10, 3, 2, 2, 2, 2} };
-    int[][] matrix3 = { {1, 2, 3, 4, 5}, {4, 4, 4, 4, 3}, {3, 2, 1, 0, 0} };
-    int[][] matrix4 = { {1, 4, 5, 10}, {18, 4, 12, 2}, {1, 2, 5, 6} };
+    int[] B = {1, 3, 5};
+    int[][] A = { {  1,  0, 12, -1 }, {  7, -2,  2,  1 }, { -5, -2,  2, -9 } };
+    int[][] C = { {  1,  2, 3, 4 }, {  2, 3,  4,  1 }, { 3, 4,  1, 2 } };
+    int[][] D = { {  1,  1, 1 }, {  2, 2, 2 }, { 3,  3, 3 } };
+    int[][] E = { {  2,  4, 2 }, {  2, 2, 2 } };
     System.out.println();
-    System.out.println(ArrayOps.sum(arr));
+    System.out.println(ArrayOps.sum(B));
     System.out.println();
-    System.out.println(ArrayOps.largest(arr));
+    System.out.println(ArrayOps.largest(B));
     System.out.println();
-    System.out.println(arrToString(ArrayOps.sumRows(matrix)));
+    System.out.println(arrToString(ArrayOps.sumRows(A)));
     System.out.println();
-    System.out.println(arrToString(ArrayOps.largestInRows(matrix)));
+    System.out.println(arrToString(ArrayOps.largestInRows(A)));
     System.out.println();
-    System.out.println(ArrayOps.sum(matrix));
+    System.out.println(ArrayOps.sum(A));
     System.out.println();
-    System.out.println(arrToString(ArrayOps.sumCols(matrix)));
+    System.out.println(arrToString(ArrayOps.sumCols(A)));
     System.out.println();
-    System.out.println(ArrayOps.isRowMagic(matrix2));
+    System.out.println(ArrayOps.isRowMagic(C));
     System.out.println();
-    System.out.println(ArrayOps.isColMagic(matrix3));
+    System.out.println(ArrayOps.isColMagic(D));
     System.out.println();
-    System.out.println(ArrayOps.isLocationMagic(matrix4, 0, 0));
+    System.out.println(ArrayOps.isLocationMagic(E, 1, 1));
   }
 
   public static String arrToString(int[] arr){
