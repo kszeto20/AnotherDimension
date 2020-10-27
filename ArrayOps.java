7dbe605a28e9d8ccb2ed36667a12abf979680b25
Kirsten Szeto <kszeto20@stuy.edu>
2020-10-26 22:09:47 -0400

    I wrote and tested largest() and it works

--- ArrayOps.java
+++ ArrayOps.java
@@ -2,6 +2,7 @@ public class ArrayOps{
   public static void main(String[] args){
     System.out.println("hello world");
   }
+
   public static int sum(int[] arr){
     int sum = 0;
     for (int i = 0; i < arr.length; i++){
@@ -10,4 +11,15 @@ public class ArrayOps{
     return sum;
   }
 
+  public static int largest(int[] arr){
+    int largest = 0;
+    for (int i = 0; i < arr.length; i++){
+      if (arr[i] > largest){
+        largest = arr[i];
+      }
+    }
+    return largest;
+  }
+
+    
 }
