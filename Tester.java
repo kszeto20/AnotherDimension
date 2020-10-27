d105b2c3c4ac412508401f74df9c10ee3f74793d
Kirsten Szeto <kszeto20@stuy.edu>
2020-10-26 22:10:49 -0400

    I added the print statement for largest() and it prints out correctly.

--- Tester.java
+++ Tester.java
@@ -1,6 +1,7 @@
 public class Tester{
   public static void main(String[] args){
-    int[] arr = {1, 3, 5, 7, 5, 2, 5, 5, 10};
+    int[] arr = {1, 3, 5, 7, 5, 30, 2, 5, 5, 10};
     System.out.println(ArrayOps.sum(arr));
+    System.out.println(ArrayOps.largest(arr));
   }
 }
